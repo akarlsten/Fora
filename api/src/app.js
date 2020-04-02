108bf1bdceb87a89a06815fd714d7fe1d5c4aa8f
Adam Karlsten <adamkarlsten@gmail.com>
2020-04-02 12:57:38 +0200

    Bootstraps keystone app

--- api/src/app.js
+++ api/src/app.js
@@ -1,19 +1,78 @@
-import { ApolloServer, gql } from 'apollo-server'
+import { Keystone } from '@keystonejs/keystone'
+import { PasswordAuthStrategy } from '@keystonejs/auth-password'
+import { Text, Checkbox, Password } from '@keystonejs/fields'
+import { GraphQLApp } from '@keystonejs/app-graphql'
+import { AdminUIApp } from '@keystonejs/app-admin-ui'
+import initialiseData from './initial-data'
 
-const typeDefs = gql`
-type Query {
-  info: String!
-}
-`
+import { MongooseAdapter as Adapter } from '@keystonejs/adapter-mongoose'
 
-const resolvers = {
-  Query: {
-    info: () => 'This is my cool api'
-  }
-}
+const PROJECT_NAME = 'test'
 
-const server = new ApolloServer({ typeDefs, resolvers })
-
-server.listen().then(({ url }) => {
-  console.log(`Server ready at ${url}`)
+const keystone = new Keystone({
+  name: PROJECT_NAME,
+  adapter: new Adapter(),
+  onConnect: initialiseData
 })
+
+// Access control functions
+const userIsAdmin = ({ authentication: { item: user } }) => Boolean(user && user.isAdmin)
+const userOwnsItem = ({ authentication: { item: user } }) => {
+  if (!user) {
+    return false
+  }
+  return { id: user.id }
+}
+
+const userIsAdminOrOwner = auth => {
+  const isAdmin = access.userIsAdmin(auth)
+  const isOwner = access.userOwnsItem(auth)
+  return isAdmin || isOwner
+}
+
+const access = { userIsAdmin, userOwnsItem, userIsAdminOrOwner }
+
+keystone.createList('User', {
+  fields: {
+    name: { type: Text },
+    email: {
+      type: Text,
+      isUnique: true
+    },
+    isAdmin: {
+      type: Checkbox,
+      // Field-level access controls
+      // Here, we set more restrictive field access so a non-admin cannot make themselves admin.
+      access: {
+        update: access.userIsAdmin
+      }
+    },
+    password: {
+      type: Password
+    }
+  },
+  // List-level access controls
+  access: {
+    read: access.userIsAdminOrOwner,
+    update: access.userIsAdminOrOwner,
+    create: access.userIsAdmin,
+    delete: access.userIsAdmin,
+    auth: true
+  }
+})
+
+const authStrategy = keystone.createAuthStrategy({
+  type: PasswordAuthStrategy,
+  list: 'User'
+})
+
+export default {
+  keystone,
+  apps: [
+    new GraphQLApp(),
+    new AdminUIApp({
+      enableDefaultRoute: true,
+      authStrategy
+    })
+  ]
+}
--- api/src/index.js
+++ api/src/index.js
@@ -2,4 +2,4 @@
 // Set options as a parameter, environment variable, or rc file.
 // eslint-disable-next-line no-global-assign
 require = require('esm')(module/* , options */)
-module.exports = require('./app.js')
+module.exports = require('./app.js').default
