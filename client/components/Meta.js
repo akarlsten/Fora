782bf84701c339983a0b05e66658d262739a7783
Adam Karlsten <adamkarlsten@gmail.com>
2020-06-07 07:37:13 +0200

    Forgot crossorigin tag on preconnect

--- client/components/Meta.js
+++ client/components/Meta.js
@@ -5,7 +5,9 @@ const Meta = () => (
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <meta charSet="utf-8" />
     <meta name="description" content="Create your community and discuss the things you care about." />
-    <link rel="preconnect" href="https://api.fora.fun" />
+    <link rel="dns-prefetch" href="https://api.fora.fun" />
+    <link rel="preconnect" href="https://api.fora.fun" crossOrigin="use-credentials" />
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com/"/>
     <link rel="shortcut icon" href="/favicon.png" />
     <title>Fora</title>
   </Head>
